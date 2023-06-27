bad556983f9701d97c154fda3da164c5e1634dfe
Anthony_Dellavecchia <dev@anthonydellavecchia.com>
2023-06-26 22:15:12 -0700

    feat(chatgpt-3.5): upgrade model from davinci to gpt-3.5-turbo

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-from flask import Flask, request
-from twilio.twiml.messaging_response import MessagingResponse
 import os
 import openai
+from flask import Flask, request
+from twilio.twiml.messaging_response import MessagingResponse
 
 openai.api_key = os.getenv("OPENAI_API_KEY")
 app = Flask(__name__)
@@ -11,16 +11,18 @@ app = Flask(__name__)
 def chatgpt():
     inb_msg = request.form['Body'].lower()
     print(inb_msg)
-    response = openai.Completion.create(
-        model="text-davinci-003",
-        prompt=inb_msg,
-        max_tokens=3000,
+    response = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=[
+            {"role": "system", "content": "You are a helpful assistant."},
+            {"role": "user", "content": inb_msg}
+        ],
         temperature=0.7
     )
 
     resp = MessagingResponse()
-    resp.message(response["choices"][0]["text"])
-    print(response["choices"][0]["text"])
+    resp.message(response["choices"][0].message.content)
+    print(response["choices"][0].message.content)
 
     return str(resp)
 
